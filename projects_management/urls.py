a8274bac00462387dad7c4363250dc586222c4e5
PolaDevX <pola.pp9291@gmail.com>
2026-03-24 23:22:53 +0200

    Admin Panel

--- projects_management/urls.py
+++ projects_management/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
+
+admin.site.site_header = ('Projects Management')
+admin.site.site_title = ('Projects Management')
+
+
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('projects.urls')),
